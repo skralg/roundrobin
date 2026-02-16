bce3ea5026f38dc545d1287c473c52e80b607e53
Jonathan Rudolph <skralg@gmail.com>
2026-02-16 13:14:32 -0800

    Full functional round-robin logic, with shard counting for added fairness.  Add/Remove characters, add/remove trackers

--- rr.js
+++ rr.js
@@ -1,8 +1,10 @@
+// Various custom functions for the Round Robin Loot Tracking tool
+
 function addChar () {
-    console.log('adding character');
     // add a new table row to the table with id="character_pool"
-    charname = prompt("Enter the name of the new member in the group");
+    charname = document.getElementById("new_char_name").value.trim();
     if (!charname) return;
+    document.getElementById("new_char_name").value = "";
     let ref = document.getElementById("character_pool");
     let row = ref.insertRow(-1);
     let id = charname.toLowerCase();
@@ -10,20 +12,30 @@ function addChar () {
     //<tr><td><input type="checkbox" value="roscoe" checked="checked"/> Roscoe</td></tr>
     let cell = row.insertCell(0);
     remove = "<span class='delchar' onclick='delChar(this)'>X</span>";
-    cell.innerHTML = remove + "<input type='checkbox' value='" + id + "'/> " + title;
+    cell.innerHTML = remove + "<input type='checkbox' value='" + id + "' checked/> " + title;
     sortTable("character_pool");
 }
 
+function delChars() {
+    if (confirm("Are you sure you want to delete all characters?\nThis will remove them from all queues.")) {
+        let table = document.getElementById("character_pool");
+        // Remove all rows except the first two (header and add char row)
+        while (table.rows.length > 2) {
+            table.deleteRow(2);
+        }
+        sortTable("character_pool");
+    }
+}
 function delChar(element) {
     if (confirm("Are you sure you want to delete this character?\nThis will remove them from all queues.")) {
         element.parentElement.parentElement.remove();
+        sortTable("character_pool");
     }
-    sortTable("character_pool");
 }
 
 function toTitleCase(str) {
-  if (!str) return "";
-  return str.toLowerCase().replace(/(^|\s)\w/g, (match) => match.toUpperCase());
+    if (!str) return "";
+    return str.toLowerCase().replace(/(^|\s)\w/g, (match) => match.toUpperCase());
 }
 
 // A function to sort table rows by the value of the checkbox
@@ -48,33 +60,60 @@ function sortTable(id) {
 }
 
 // Create a new Round-Robin Queue for the current group
-function addQueue(shards=false) {
-    queueName = prompt("Enter the name for the new Queue");
-    console.log('adding queue ' + queueName);
+function addQueue(queueName="", shards=false) {
+    if (!queueName) {
+        queueName = prompt("Enter the name for the new Queue");
+        if (!queueName) return;
+    }
+    characters = getCheckedCharacterList();
+    if (characters.length === 0) {
+        alert("Please select at least one character from the Character List to add a queue.");
+        return;
+    }
     tbl = document.createElement("table");
     tbl.classList.add("queue");
+    tbl.dataset.lastWinner = "";
     document.getElementById("qc").appendChild(tbl);
     // Header row
     row = tbl.insertRow(-1);
     th = document.createElement("th");
     th.colSpan = 2;
-    th.innerHTML="<span class='delchar' onclick='delQueue(this)'>X</span> " + queueName;
+    deleteQ = "<span class='delchar' onclick='delQueue(this)'>X</span> ";
+    resetQ = " <span class='delchar' onclick='resetQueue(this)' alt='Reset Tracker'>↺</span> ";
+    th.innerHTML = deleteQ + queueName + resetQ;
     row.appendChild(th);
-    characters = getCheckedCharacterList();
+    // Suggestion row
+    row = tbl.insertRow(-1);
+    th = document.createElement("th");
+    th.colSpan = 2;
+    th.classList.add("suggest");
+    th.innerText = "Next: Everyone click NEED";
+    row.appendChild(th);
+    
     characters.forEach(char => {
         row = tbl.insertRow(-1);
         cell = row.insertCell(0);
         cell.innerText = toTitleCase(char);
         controls = row.insertCell(1);
         if (shards) {
-            controls.innerHTML = "<input class='count' size='1' value='0'/> <button class='tools' onclick='loot2(this)'>+2</button> <button class='tools' onclick='loot5(this)'>+5</button><input type='checkbox' checked/>";
+            controls.innerHTML = "<input class='count' size='2' value='0'/> <button class='tools' onclick='loot2(this)'>+2</button> <button class='tools' onclick='loot5(this)'>+5</button><input type='checkbox' checked/>";
         } else {
-            controls.innerHTML = "<input class='count' size='1' value='0'/> <button class='tools' onclick='loot(this)'>+</button><input type='checkbox' checked/>";
+            controls.innerHTML = "<input class='count' size='2' value='0'/> <button class='tools' onclick='loot(this)'>+</button><input type='checkbox' checked/>";
         }
     });
+}
 
-    
-
+function resetQueue(element) {
+    if (confirm("Are you sure you want to reset this queue?")) {
+        queueTable = element.parentElement.parentElement.parentElement.parentElement;
+        inputs = queueTable.querySelectorAll("input.count");
+        inputs.forEach(input => input.value = "0");
+        queueTable.dataset.lastWinner = "";
+        // Update suggestion text to the next player at the top of the list
+        suggestion = queueTable.querySelector(".suggest");
+        suggestion.innerText = "Next: Everyone click NEED"
+        sortQueue(queueTable);
+    }
 }
 
 function getCheckedCharacterList() {
@@ -102,21 +141,37 @@ function loot(element, count=1) {
     value = parseInt(input.value) || 0;
     value += count || 1;
     input.value = value;
+    row = element.parentElement.parentElement;
+    player = row.cells[0].innerText;
     parentTable = element.parentElement.parentElement.parentElement.parentElement;
+    if (parentTable.dataset.lastWinner == "") parentTable.dataset.lastWinner = player;
     sortQueue(parentTable);
+    // Update suggestion text to the next player at the top of the list
+    suggestion = parentTable.querySelector(".suggest");
+    if (suggestion) {
+        nextPlayer = parentTable.querySelector("tr:nth-child(3) td:first-child").innerText;
+        suggestion.innerText = "Next: " + nextPlayer;
+    }
 }
 
 function sortQueue(queueTable) {
     // Sort the rows of the queue table based on the value of the count input,
     // in ascending order, then by cell 0 alphabetically
     tbody = queueTable.tBodies[0] || queueTable;
-    rows = Array.from(tbody.rows).slice(1); // skip header
+    rows = Array.from(tbody.rows).slice(2); // skip headers
+    lastWinner = queueTable.dataset.lastWinner.toLowerCase();
     rows.sort((a, b) => {
         aCount = parseInt(a.querySelector("input.count").value) || 0;
         bCount = parseInt(b.querySelector("input.count").value) || 0;
         if (bCount !== aCount) return aCount - bCount; // sort by count asc
+        // Round robin after last winner
         aName = a.cells[0].innerText.toLowerCase();
         bName = b.cells[0].innerText.toLowerCase();
+        if (lastWinner) {
+            //if name is after last winner alphabetically, it should sort higher than names that are before the last winner
+            if (aName.localeCompare(lastWinner) > 0 && bName.localeCompare(lastWinner) < 0) return -1;
+            if (aName.localeCompare(lastWinner) < 0 && bName.localeCompare(lastWinner) > 0) return 1;
+        }
         return aName.localeCompare(bName); // then by name asc
     });
     rows.forEach(row => tbody.appendChild(row));
